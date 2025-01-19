22dbd4e8c4c1898c762c2d2ea8a25a50192601dd
yvishal30 <30vishalyadav@gmail.com>
2025-01-19 19:29:42 +0530

    Menu Item added to cart Success Message

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,23 +1,18 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Add_To_Cart Page</title>
-  </head>
-  <body>
-    
-  <?php
+<?php
 session_start();
 
-// Get the posted data
+// Set the header for JSON response
+header('Content-Type: application/json');
+
+// Decode the posted JSON data
 $data = json_decode(file_get_contents('php://input'), true);
 
+if (!isset($data['productName'], $data['productPrice'], $data['quantity'], $data['totalPrice'], $data['productImageURL'])) {
+    echo json_encode(['message' => 'Invalid data sent to the server.']);
+    exit;
+}
+
+// Initialize the cart if not already
 if (!isset($_SESSION['cart'])) {
     $_SESSION['cart'] = [];
 }
@@ -34,16 +29,3 @@ $_SESSION['cart'][] = [
 // Respond with a success message
 echo json_encode(['message' => 'Item added to cart successfully!']);
 ?>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
